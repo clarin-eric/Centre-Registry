292efaf1b7557655a486781db2e50d38de0b7649
Sander Maijers <S.N.Maijers@gmail.com>
2016-07-15 14:53:18 +0200

    Improve tests
    - `lxml` seems to have changed its API in recent times. Refactor to make XSD
      validation work again.
    - Fix: crash if `CONTINUOUS_INTEGRATION` env var undeclared.
    - Fix: import `Remote` not additionally but alternatively.
    - Style: apply autoformatting.
    
    The tests should now work under more configurations.

--- centre-registry-app/centre_registry/test_api.py
+++ centre-registry-app/centre_registry/test_api.py
@@ -1,88 +1,84 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from os.path import join
+from pkg_resources import resource_string
+from traceback import print_exc
 import unittest
+
+from django.core import management
+from django.test import Client
+from django import setup
+from lxml.etree import DocumentInvalid
+from lxml.etree import fromstring
+from lxml.etree import parse
+from lxml.etree import XMLSchema
+from lxml.etree import XMLSyntaxError
+from lxml.etree import XPath
 
 
 class APITestCase(unittest.TestCase):
-    # fixtures = ['test_data']
-    serialized_rollback = False
-    # available_apps = ['centre_registry']
-
     @classmethod
     def setUpClass(cls):
-        from django import setup
-        from django.core import management
-
         setup()
-        management.call_command('loaddata', 'test_data.json', verbosity=1, noinput=True)
+        management.call_command('loaddata', 'test_data.json', verbosity=1,
+                                noinput=True)
         super(APITestCase, cls).setUpClass()
 
     # Tests for API v1
     def test_all_centres(self):
-        from django.test import Client
-        from lxml.etree import fromstring, XPath
-
         client = Client()
         response = client.get('/restxml/', secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-Type'], 'application/xml')
 
         xml_tree = fromstring(response.content)
-        centre_info_url_xpath = XPath('/Centers/CenterProfile/Center_id_link/text()')
+        centre_info_url_xpath = XPath(
+            '/Centers/CenterProfile/Center_id_link/text()')
         centre_info_urls = centre_info_url_xpath(xml_tree)
         response = client.get(centre_info_urls[0], secure=True)
         self.assertEqual(response.status_code, 200)
 
-
     def test_centre(self):
-        from django.test import Client
-        from lxml.etree import fromstring, XMLParser, XMLSchema, XMLSyntaxError
-        from os.path import join
-        from pkg_resources import resource_string
-        from traceback import print_exc
-
         client = Client()
         response = client.get('/restxml/1/', secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-Type'], 'application/xml')
 
-        schema_root = fromstring(resource_string(__name__, join('data', 'CenterProfile.xsd')))
+        schema_root = fromstring(
+            resource_string(__name__, join('data', 'CenterProfile.xsd')))
         schema = XMLSchema(schema_root)
 
-        parser = XMLParser(schema=schema)
         try:
-            fromstring(response.content, parser)
-        except XMLSyntaxError:
+            xml_doc = fromstring(response.content)
+            schema.assertValid(xml_doc)
+        except (XMLSyntaxError, DocumentInvalid):
             print_exc()
             self.fail()
 
     # Tests for API v2
     def test_centres_kml(self):
-        from django.test import Client
-        from lxml.etree import fromstring, parse, XMLParser, XMLSchema, XMLSyntaxError
-        from traceback import print_exc
-
         client = Client()
         response = client.get('/api/KML/', secure=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response['Content-Type'], 'application/vnd.google-earth.kml+xml')
+        self.assertEqual(response['Content-Type'],
+                         'application/vnd.google-earth.kml+xml')
 
-        # It is necessary to load this KML XSD over HTTP as it imports two other XSDs using relative names, which
-        # does not work well with Python package resources, that should not be located to an absolute location.
+        # It is necessary to load this KML XSD over HTTP as it imports two
+        # other XSDs using relative names, which
+        # does not work well with Python package resources, that should not
+        # be located to an absolute location.
         schema_doc = parse("http://www.opengis.net/kml/2.2")
         schema = XMLSchema(schema_doc)
 
-        parser = XMLParser(schema=schema)
         try:
-            fromstring(response.content, parser)
-        except XMLSyntaxError:
+            xml_doc = fromstring(response.content)
+            schema.assertValid(xml_doc)
+        except (XMLSyntaxError, DocumentInvalid):
             print_exc()
             self.fail()
 
     def test_model(self):
-        from django.test import Client
-
         client = Client()
         response = client.get('/api/model/Centre', secure=True)
         self.assertEqual(response.status_code, 200)
--- centre-registry-app/centre_registry/test_ui.py
+++ centre-registry-app/centre_registry/test_ui.py
@@ -3,26 +3,21 @@
 
 from os import environ
 
-from django.test import LiveServerTestCase
-
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 try:
     from selenium.webdriver.firefox.webdriver import WebDriver
 except ImportError:
     from selenium.webdriver.chrome.webdriver import WebDriver
-else:
-    from selenium.webdriver import Remote
 
 
-class SystemTestCase(LiveServerTestCase):
+class SystemTestCase(StaticLiveServerTestCase):
     fixtures = ['test_data.json']
-    serialized_rollback = False
-
-    # available_apps = ['centre_registry']
 
     @classmethod
     def setUpClass(cls):
-        # from django.core import management
-        if environ['CONTINUOUS_INTEGRATION'].lower() == 'true':
+        is_ci = (environ.get('CONTINUOUS_INTEGRATION') or '').lower() == 'true'
+        if is_ci:
+            from selenium.webdriver import Remote
             desired_capabilities = {
                 "tunnel-identifier": environ["TRAVIS_JOB_NUMBER"],
                 "build": environ["TRAVIS_BUILD_NUMBER"],
@@ -35,96 +30,81 @@ class SystemTestCase(LiveServerTestCase):
                                   .format(hub_url=hub_url))
         else:
             cls.selenium = WebDriver()
-        # management.call_command('loaddata', 'test_data.json', verbosity=1,
-        # noinput=True)
         super(SystemTestCase, cls).setUpClass()
 
+    @classmethod
+    def tearDownClass(cls):
+        super(SystemTestCase, cls).tearDownClass()
+        cls.selenium.quit()
 
-@classmethod
-def tearDownClass(cls):
-    super(SystemTestCase, cls).tearDownClass()
-    cls.selenium.quit()
+    def test_admin(self):
+        self.selenium.get(self.live_server_url + '/admin')
 
+        self.selenium.find_element_by_id('id_username')
 
-def test_admin(self):
-    self.selenium.get(self.live_server_url + '/admin')
+    def test_about(self):
+        self.selenium.get(self.live_server_url + '/about')
 
-    self.selenium.find_element_by_id('id_username')
+        body = self.selenium.find_element_by_tag_name('body')
+        self.assertIn('Sander Maijers', body.text)
 
+    def test_all_centres(self):
+        self.selenium.get(self.live_server_url + '/all_centres')
 
-def test_about(self):
-    self.selenium.get(self.live_server_url + '/about')
+        table = self.selenium.find_element_by_id('all_centres')
+        table.find_element_by_tag_name('tr')
 
-    body = self.selenium.find_element_by_tag_name('body')
-    self.assertIn('Sander Maijers', body.text)
+    def test_centre(self):
+        self.selenium.get(self.live_server_url + '/centre/1')
 
+        body = self.selenium.find_element_by_tag_name('body')
+        self.assertIn('Karls', body.text)
 
-def test_all_centres(self):
-    self.selenium.get(self.live_server_url + '/all_centres')
+    def test_centres_contacts(self):
+        self.selenium.get(self.live_server_url + '/centres_contacts')
 
-    table = self.selenium.find_element_by_id('all_centres')
-    table.find_element_by_tag_name('tr')
+        table = self.selenium.find_element_by_id('centres_contacts')
+        table.find_element_by_tag_name('tr')
 
+    def test_consortia(self):
+        self.selenium.get(self.live_server_url + '/consortia')
 
-def test_centre(self):
-    self.selenium.get(self.live_server_url + '/centre/1')
+        table = self.selenium.find_element_by_id('consortia')
+        table.find_element_by_tag_name('tr')
 
-    body = self.selenium.find_element_by_tag_name('body')
-    self.assertIn('Karls', body.text)
+    def test_contacting(self):
+        self.selenium.get(self.live_server_url + '/contacting')
 
+        body = self.selenium.find_element_by_tag_name('body')
+        self.assertIn('Contacting', body.text)
 
-def test_centres_contacts(self):
-    self.selenium.get(self.live_server_url + '/centres_contacts')
+    def test_contact(self):
+        self.selenium.get(self.live_server_url + '/contact/1')
 
-    table = self.selenium.find_element_by_id('centres_contacts')
-    table.find_element_by_tag_name('tr')
+        table = self.selenium.find_element_by_id('contact')
+        self.assertIn('ePPN', table.text)
 
+    def test_fcs(self):
+        self.selenium.get(self.live_server_url + '/fcs')
 
-def test_consortia(self):
-    self.selenium.get(self.live_server_url + '/consortia')
+        body = self.selenium.find_element_by_tag_name('body')
+        self.assertIn('FCS endpoint', body.text)
 
-    table = self.selenium.find_element_by_id('consortia')
-    table.find_element_by_tag_name('tr')
+    def test_map(self):
+        self.selenium.get(self.live_server_url + '/map')
 
+        body = self.selenium.find_element_by_tag_name('body')
+        self.assertIn('geographical overview', body.text)
 
-def test_contacting(self):
-    self.selenium.get(self.live_server_url + '/contacting')
+    def test_oai_pmh(self):
+        self.selenium.get(self.live_server_url + '/oai_pmh')
 
-    body = self.selenium.find_element_by_tag_name('body')
-    self.assertIn('Contacting', body.text)
+        table = self.selenium.find_element_by_id('oai-pmh_endpoints')
+        table.find_element_by_tag_name('tr')
 
+    def test_spf(self):
+        self.selenium.get(self.live_server_url + '/spf')
 
-def test_contact(self):
-    self.selenium.get(self.live_server_url + '/contact/1')
-
-    table = self.selenium.find_element_by_id('contact')
-    self.assertIn('ePPN', table.text)
-
-
-def test_fcs(self):
-    self.selenium.get(self.live_server_url + '/fcs')
-
-    body = self.selenium.find_element_by_tag_name('body')
-    self.assertIn('FCS endpoint', body.text)
-
-
-def test_map(self):
-    self.selenium.get(self.live_server_url + '/map')
-
-    body = self.selenium.find_element_by_tag_name('body')
-    self.assertIn('geographical overview', body.text)
-
-
-def test_oai_pmh(self):
-    self.selenium.get(self.live_server_url + '/oai_pmh')
-
-    table = self.selenium.find_element_by_id('oai-pmh_endpoints')
-    table.find_element_by_tag_name('tr')
-
-
-def test_spf(self):
-    self.selenium.get(self.live_server_url + '/spf')
-
-    table = self.selenium.find_element_by_id(
-        'saml_service_providers_and_identity_federations')
-    table.find_element_by_tag_name('tr')
+        table = self.selenium.find_element_by_id(
+            'saml_service_providers_and_identity_federations')
+        table.find_element_by_tag_name('tr')
