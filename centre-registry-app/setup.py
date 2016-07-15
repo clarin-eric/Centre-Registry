7699ac6362afb2bcc33936870d12e43007b92ebd
Sander Maijers <S.N.Maijers@gmail.com>
2016-07-15 12:19:14 +0200

    Style: PEP8
    Simplify import statements.

--- centre-registry-app/setup.py
+++ centre-registry-app/setup.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-__version__ = '2.1.1'
-
-from os import chdir as os_chdir
-from os import pardir as os_pardir
-import os.path
+from os.path import abspath
+from os.path import join
+from os.path import normpath
+from os import chdir
+from os import pardir
 from setuptools import setup
 from sys import version_info as sys__version_info
 
+__version__ = '2.1.1'
 
 if sys__version_info < (3, 4):
-    raise RuntimeError('ERROR: under Python version {0}.{1}, while version >= 3.4 required. '
-                       .format(str(sys__version_info.major),
-                               str(sys__version_info.minor)))
+    raise RuntimeError(
+        'ERROR: under Python version {0}.{1}, while version >= 3.4 required. '
+        .format(str(sys__version_info.major),
+                str(sys__version_info.minor)))
 
 setup_requires = ['wheel>=0.24']
-install_requires = ['django>=1.8,<1.9', 'django-debug-toolbar>=1.3,<1.4', 'django-grappelli>=2.6.4,<2.7']
+install_requires = ['django>=1.8,<1.9', 'django-debug-toolbar>=1.3,<1.4',
+                    'django-grappelli>=2.6.4,<2.7']
 test_requires = ['lxml>=3.4.3,<3.5', 'selenium>=2.45.0,<2.46.0']
 
-os_chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os_pardir)))
+chdir(normpath(join(abspath(__file__), pardir)))
 setup(
     name='centre_registry_app',
     version=__version__,
