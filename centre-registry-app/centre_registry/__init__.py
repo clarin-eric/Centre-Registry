646cf5192a0775da9bb945c4265a2f91df70fa47
Sander Maijers <S.N.Maijers@gmail.com>
2016-07-25 18:52:30 +0200

    Remove legacy `default_app_config`
    See https://docs.djangoproject.com/en/1.9/ref/applications/#configuring-applications

--- centre-registry-app/centre_registry/__init__.py
+++ centre-registry-app/centre_registry/__init__.py
@@ -1,8 +1,2 @@
-import centre_registry.apps
-
 __all__ = ['admin', 'apps', 'context_processors', 'models', 'test_api',
            'test_ui', 'views_ui', 'views_api']
-
-default_app_config = '{0}.{1}'.format(
-    centre_registry.apps.__name__,
-    centre_registry.apps.CentreRegistryConfig.__qualname__)
