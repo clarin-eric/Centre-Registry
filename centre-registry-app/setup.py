2def5e3d0b382bab9208c62bc32082ae8450debc
Sander Maijers <S.N.Maijers@gmail.com>
2016-10-31 09:12:12 +0100

    Clean up setuptools scripts
    - Use fixed version deps.
    - Update deps' versions.
    - Update wheel to `py35`.
    - Apply pre-commit hooks.

--- centre-registry-app/setup.py
+++ centre-registry-app/setup.py
@@ -7,13 +7,10 @@ from os.path import normpath
 
 from setuptools import setup
 
-__version__ = '2.1.1'
+__version__ = '2.2.0'
 
-SETUP_REQUIRES = ['wheel>=0.24']
-INSTALL_REQUIRES = ['django>=1.8,<1.9', 'django-debug-toolbar>=1.3,<1.4',
-                    'django-grappelli>=2.6.4,<2.7']
-TEST_REQUIRES = ['lxml>=3.4.3,<3.5', 'selenium>=2.45.0,<2.46.0']
-
+INSTALL_REQUIRES = ['Django==1.9.10', 'django-debug-toolbar==1.6']
+TEST_REQUIRES = ['lxml==3.6.4', 'selenium==2.53.6']
 chdir(normpath(join(abspath(__file__), pardir)))
 setup(
     name='centre_registry_app',
@@ -21,7 +18,6 @@ setup(
     packages=['centre_registry'],
     include_package_data=True,
     install_requires=INSTALL_REQUIRES,
-    setup_requires=SETUP_REQUIRES,
     test_requires=TEST_REQUIRES,
     license='GPLv3',
     description='CLARIN Centre Registry, a Django application. ',
