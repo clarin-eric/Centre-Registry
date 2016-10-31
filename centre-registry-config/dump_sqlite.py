e1cffe7053a56ac3a317113b9bd0cc19af022ce6
Sander Maijers <S.N.Maijers@gmail.com>
2016-10-31 09:16:38 +0100

    Remove unnecessary script file

--- centre-registry-config/dump_sqlite.py
+++
@@ -1,6 +0,0 @@
-# Convert file existing_db.db to SQL dump file dump.sql
-from sqlite3 import connect
-
-with open('dump.sql', 'w') as f:
-    for line in connect('Centre_Registry.sqlite').iterdump():
-        f.write('%s\n' % line)
