21f6f33bb1bd5b1f314f19a60d0f1641aea46f4c
andmor- <amoreira.mail@gmail.com>
2018-10-30 17:34:09 +0100

    Bumped version to 2.2.3

--- centre-registry-app/setup.py
+++ centre-registry-app/setup.py
@@ -7,7 +7,7 @@ from os.path import normpath
 
 from setuptools import setup
 
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 
 INSTALL_REQUIRES = ['Django==1.9.13', 'django-debug-toolbar==1.10']
 TEST_REQUIRES = ['lxml==4.2.4', 'selenium==2.53.6']
