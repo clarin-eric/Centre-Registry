6481cdd8c1e74a037acc0ef1779b2db4a6670597
andmor- <amoreira.mail@gmail.com>
2018-12-04 17:13:51 +0100

    Downgraded Django debug toolbar to last compatible version
    1.9.1 is the last version of the Django debug toolbar fully compatible with Django 1.9.13

--- centre-registry-app/setup.py
+++ centre-registry-app/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 __version__ = '2.2.4'
 
-INSTALL_REQUIRES = ['Django==1.9.13', 'django-debug-toolbar==1.10']
+INSTALL_REQUIRES = ['Django==1.9.13', 'django-debug-toolbar==1.9.1']
 TEST_REQUIRES = ['lxml==4.2.4', 'selenium==2.53.6']
 chdir(normpath(join(abspath(__file__), pardir)))
 setup(
