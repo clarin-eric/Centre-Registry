054529b5823629d252214206c04ebba8c9138b7c
andmore <andre@clarin.eu>
2019-11-07 17:09:46 +0100

    Update manage.py

--- Centre-Registry-config/manage.py
+++ Centre-Registry-config/manage.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
-import os
 from sys import argv
 
 from django.core.management import execute_from_command_line
 
-sauce_k = os.environ['SAUCE_ACCESS_KEY']
-sauce_u = os.environ['SAUCE_USERNAME']
-
 if __name__ == "__main__":
-    print("Sauce card: |{} {}|".format(sauce_k[:len(sauce_k)//2], sauce_k[len(sauce_k)//2:]))
-    print("Sauce person: |{} {}|".format(sauce_u[:len(sauce_u)//2], sauce_u[len(sauce_u)//2:]))
     execute_from_command_line(argv)
