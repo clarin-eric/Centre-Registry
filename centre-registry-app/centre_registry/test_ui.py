a3ba1d76d214270543dd8171d805266e7a3dfbb8
andmor <amoreira.mail@gmail.com>
2019-11-14 18:22:49 +0100

    Travis test

--- centre-registry-app/centre_registry/test_ui.py
+++ centre-registry-app/centre_registry/test_ui.py
@@ -19,7 +19,7 @@ class SystemTestCase(StaticLiveServerTestCase):
         is_ci = (environ.get('TRAVIS') or '').lower() == 'true'
         if is_ci:
             from selenium.webdriver import Remote
-            hub_url = ("{username:s}:{access_key:s}@localhost:4445"
+            hub_url = ("{username:s}:{access_key:s}@ondemand.eu-central-1.saucelabs.com"
                 .format(
                 username=environ["SAUCE_USERNAME"],
                 access_key=environ["SAUCE_ACCESS_KEY"]))
@@ -33,7 +33,7 @@ class SystemTestCase(StaticLiveServerTestCase):
             }
             cls.selenium = Remote(
                 desired_capabilities=desired_capabilities,
-                command_executor="http://{hub_url:s}/wd/hub"
+                command_executor="https://{hub_url:s}/wd/hub"
                     .format(hub_url=hub_url))
         else:
             cls.selenium = WebDriver()
