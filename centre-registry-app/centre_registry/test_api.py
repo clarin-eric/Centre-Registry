f7f354975789bf92cd9b5a2c407040c14c7796cf
MichalGawor <michal.j.gawor@gmail.com>
2020-01-23 14:50:27 +0100

    api/model/M tests

--- centre-registry-app/centre_registry/test_api.py
+++ centre-registry-app/centre_registry/test_api.py
@@ -3,8 +3,11 @@ from os.path import join
 from traceback import print_exc
 
 from django import setup
+from django.core import serializers
+from django.http import JsonResponse
 from django.test import TestCase
 from django.test import Client
+import json
 from lxml.etree import DocumentInvalid
 from lxml.etree import fromstring
 from lxml.etree import parse
@@ -12,6 +15,18 @@ from lxml.etree import XMLSchema
 from lxml.etree import XMLSyntaxError
 from lxml.etree import XPath
 from pkg_resources import resource_string
+
+from centre_registry.models import Centre
+from centre_registry.models import CentreType
+from centre_registry.models import Consortium
+from centre_registry.models import AssessmentDates
+from centre_registry.models import Contact
+from centre_registry.models import FCSEndpoint
+from centre_registry.models import MetadataFormat
+from centre_registry.models import OAIPMHEndpoint
+from centre_registry.models import SAMLIdentityFederation
+from centre_registry.models import SAMLServiceProvider
+from centre_registry.models import URLReference
 
 
 class APITestCase(TestCase):
@@ -79,8 +94,82 @@ class APITestCase(TestCase):
             print_exc()
             self.fail()
 
-    def test_model(self):
+    def test_get_model_centre(self):
         client = Client()
         response = client.get('/api/model/Centre', secure=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response['Content-Type'], 'application/json')
+
+        centres_in_response = json.loads(response.content)
+        self.assertEqual(centres_in_response, json.loads(serializers.serialize('json', Centre.objects.all())))
+
+    def test_get_model_centretype(self):
+        client = Client()
+        response = client.get('/api/model/CentreType', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        centre_types_in_response = json.loads(response.content)
+        self.assertEqual(centre_types_in_response, json.loads(serializers.serialize('json', CentreType.objects.all())))
+
+    def test_get_model_contact(self):
+        client = Client()
+        response = client.get('/api/model/Contact', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        contacts_in_response = json.loads(response.content)
+        self.assertEqual(contacts_in_response, json.loads(serializers.serialize('json', Contact.objects.all())))
+
+    def test_get_model_consortium(self):
+        client = Client()
+        response = client.get('/api/model/Consortium', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        consortiums_in_response = json.loads(response.content)
+        self.assertEqual(consortiums_in_response, json.loads(serializers.serialize('json', Consortium.objects.all())))
+
+    def test_get_model_fcsendpoint(self):
+        client = Client()
+        response = client.get('/api/model/FCSEndpoint', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        fcsendpoints_in_response = json.loads(response.content)
+        self.assertEqual(fcsendpoints_in_response, json.loads(serializers.serialize('json', FCSEndpoint.objects.all())))
+
+    def test_get_model_urlreference(self):
+        client = Client()
+        response = client.get('/api/model/URLReference', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        urlreferences_in_response = json.loads(response.content)
+        self.assertEqual(urlreferences_in_response, json.loads(serializers.serialize('json', URLReference.objects.all())))
+
+    def test_get_model_metadataformat(self):
+        client = Client()
+        response = client.get('/api/model/MetadataFormat', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        metadataformat_in_response = json.loads(response.content)
+        self.assertEqual(metadataformat_in_response, json.loads(serializers.serialize('json', MetadataFormat.objects.all())))
+
+    def test_get_model_oaipmhendpoint(self):
+        client = Client()
+        response = client.get('/api/model/OAIPMHEndpoint', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        oaipmhendpoint_in_response = json.loads(response.content)
+        self.assertEqual(oaipmhendpoint_in_response, json.loads(serializers.serialize('json', OAIPMHEndpoint.objects.all())))
+
+    def test_get_model_samlidentityfederation(self):
+        client = Client()
+        response = client.get('/api/model/SAMLIdentityFederation', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        samlidentityfederation_in_response = json.loads(response.content)
+        self.assertEqual(samlidentityfederation_in_response, json.loads(serializers.serialize('json', SAMLIdentityFederation.objects.all())))
+
+    def test_get_model_samlserviceprovider(self):
+        client = Client()
+        response = client.get('/api/model/SAMLServiceProvider', secure=True)
+        self.assertEqual(response.status_code, 200)
+
+        samlserviceprovider_in_response = json.loads(response.content)
+        self.assertEqual(samlserviceprovider_in_response, json.loads(serializers.serialize('json', SAMLServiceProvider.objects.all())))
