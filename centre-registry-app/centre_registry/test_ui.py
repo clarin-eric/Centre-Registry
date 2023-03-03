45ae8bb60135f38718a7217457647319b8bc11a3
André Moreira <amoreira.mail@gmail.com>
2023-03-03 15:28:57 +0100

    Remove keyword arguments

--- centre-registry-app/centre_registry/test_ui.py
+++ centre-registry-app/centre_registry/test_ui.py
@@ -69,76 +69,76 @@ class SystemTestCase(StaticLiveServerTestCase):
     def test_admin(self):
         self.selenium.get(self.live_server_url + '/admin')
 
-        self.selenium.find_element(by=By.ID, value='id_username')
+        self.selenium.find_element(By.ID, 'id_username')
 
     def test_about(self):
         self.selenium.get(self.live_server_url + '/about')
 
-        body = self.selenium.find_element(by=By.TAG_NAME, value='body')
+        body = self.selenium.find_element(By.TAG_NAME, 'body')
         self.assertIn('Sander Maijers', body.text)
 
     def test_all_centres(self):
         self.selenium.get(self.live_server_url + '/all_centres')
 
-        table = self.selenium.find_element(by=By.ID, value='all_centres')
-        table.find_element(by=By.TAG_NAME, value='tr')
+        table = self.selenium.find_element(By.ID, 'all_centres')
+        table.find_element(By.TAG_NAME, 'tr')
 
     def test_centre(self):
         self.selenium.get(self.live_server_url + '/centre/1')
 
-        body = self.selenium.find_element(by=By.TAG_NAME, value='body')
+        body = self.selenium.find_element(By.TAG_NAME, 'body')
         self.assertIn('Karls', body.text)
 
     def test_contact_in_centre(self):
         self.selenium.get(self.live_server_url + '/centre/1')
-        body = self.selenium.find_element(by=By.TAG_NAME, value='body')
+        body = self.selenium.find_element(By.TAG_NAME, 'body')
         self.assertIn('Margarethe Weber', body.text)
 
     def test_centres_contacts(self):
         self.selenium.get(self.live_server_url + '/centres_contacts')
 
-        table = self.selenium.find_element(by=By.ID, value='centres_contacts')
-        table.find_element(by=By.TAG_NAME, value='tr')
+        table = self.selenium.find_element(By.ID, 'centres_contacts')
+        table.find_element(By.TAG_NAME, 'tr')
 
     def test_consortia(self):
         self.selenium.get(self.live_server_url + '/consortia')
 
-        table = self.selenium.find_element(by=By.ID, value='consortia')
-        table.find_element(by=By.TAG_NAME, 'tr')
+        table = self.selenium.find_element(By.ID, 'consortia')
+        table.find_element(By.TAG_NAME, 'tr')
 
     def test_contacting(self):
         self.selenium.get(self.live_server_url + '/contacting')
 
-        body = self.selenium.find_element(by=By.TAG_NAME, value='body')
+        body = self.selenium.find_element(By.TAG_NAME, 'body')
         self.assertIn('Contacting', body.text)
 
     def test_contact(self):
         self.selenium.get(self.live_server_url + '/contact/1')
 
-        table = self.selenium.find_element(by=By.ID, value='contact')
+        table = self.selenium.find_element(By.ID, 'contact')
         self.assertIn('ePPN', table.text)
 
     def test_fcs(self):
         self.selenium.get(self.live_server_url + '/fcs')
 
-        body = self.selenium.find_element(by=By.TAG_NAME, value='body')
+        body = self.selenium.find_element(By.TAG_NAME, 'body')
         self.assertIn('FCS endpoint', body.text)
 
     def test_map(self):
         self.selenium.get(self.live_server_url + '/map')
 
-        body = self.selenium.find_element(by=By.TAG_NAME, value='body')
+        body = self.selenium.find_element(By.TAG_NAME, 'body')
         self.assertIn('geographical overview', body.text)
 
     def test_oai_pmh(self):
         self.selenium.get(self.live_server_url + '/oai_pmh')
 
-        table = self.selenium.find_element(by=By.ID, value='oai-pmh_endpoints')
-        table.find_element(by=By.TAG_NAME, 'tr')
+        table = self.selenium.find_element(By.ID, 'oai-pmh_endpoints')
+        table.find_element(By.TAG_NAME, 'tr')
 
     def test_spf(self):
         self.selenium.get(self.live_server_url + '/spf')
 
-        table = self.selenium.find_element(by=By.ID, 
-            value='saml_service_providers_and_identity_federations')
-        table.find_element(by=By.TAG_NAME, value='tr')
+        table = self.selenium.find_element(By.ID, 
+            'saml_service_providers_and_identity_federations')
+        table.find_element(By.TAG_NAME, 'tr')
