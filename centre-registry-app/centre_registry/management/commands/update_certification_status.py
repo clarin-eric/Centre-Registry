1541b08c6ce87eb6980819f4808923a6a6221ad1
MichalGawor <michal.j.gawor@gmail.com>
2025-04-09 17:19:55 +0200

    stash

--- centre-registry-app/centre_registry/management/commands/update_certification_status.py
+++ centre-registry-app/centre_registry/management/commands/update_certification_status.py
@@ -14,47 +14,36 @@ class Command(BaseCommand):
     help = 'Command to update certification status based on expiration date'
 
     def handle(self, *args, **options):
-        today_date = timezone.now()
+        today_date = timezone.now().date()
         print("TODAY DATE")
         print(today_date)
 
         centres = Centre.objects.all()
         expired_status_id = CertificationStatus.objects.get(status="Pending (recertification)")
+        certified_status_id = CertificationStatus.objects.get(status="Certified")
         outdated_centres = []
 
         for centre in centres:
-            centre_types = centre.type.all()
-            assessment_dates = centre.assessmentdates.all()
-            print(assessment_dates)
-            logging.critical(assessment_dates)
-            logging.critical("HERE")
-            for assessment_date in assessment_dates:
-                logging.critical("HERE1")
-                print(assessment_date)
-                assessment_date_type = assessment_date.type
-                if assessment_date_type in centre_types:
-                    due_date = assessment_date.duedate
-                    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-                    print(due_date)
-                    print(today_date)
-                    print(due_date > today_date)
-                    logging.critical("DUE")
-                    logging.critical(due_date)
-                    logging.critical("TODAY")
-                    logging.critical(today_date)
-                    logging.critical(due_date > today_date)
-                    if due_date > today_date:
-                        if not centre.requires_manual_certificate_validation:
-                            centre.type_certification_status = expired_status_id
-                            centre.requires_manual_certificate_validation = True
-                            outdated_centres.append(centre.name)
-                            centre.save()
+            type_certification_statuses = centre.type_certification_status_fk.all()
+            for type_certification_status in type_certification_statuses:
+                assessment_date = type_certification_status.assessmentdate
+                due_date = assessment_date.duedate
+                if due_date < today_date and type_certification_status.certification_status == certified_status_id:
+                    logging.critical(expired_status_id)
+                    type_certification_status.certification_status = expired_status_id
+                    type_certification_status.assessmentdate = assessment_date
+                    type_certification_status.save()
+
+                    centre.requires_manual_certificate_validation = True
+                    outdated_centres.append(centre.name)
+                    centre.save()
 
         if outdated_centres:
             outdated_centres = '\n'.join(outdated_centres)
             subject = "Centres certification expired"
             message = "Following centre has their assessment dates expired today:" + outdated_centres
 
+            print("###MESSAGE###")
             print(message)
             send_mail(subject=subject,
                       message=message,
