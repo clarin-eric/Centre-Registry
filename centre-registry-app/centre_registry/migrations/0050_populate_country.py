04d54f1ce7ae291d2ce77d2165857fecf9705d93
MichalGawor <michal.j.gawor@gmail.com>
2026-04-09 20:09:22 +0200

    proper migration chaining

--- centre-registry-app/centre_registry/migrations/0050_populate_country.py
+++ centre-registry-app/centre_registry/migrations/0050_populate_country.py
@@ -7,17 +7,18 @@ def forwards(apps, schema_editor):
     # CentreModel and OrganisationModel are classes, therefore CamelCase
     ConsortiumModel = apps.get_model("centre_registry", "Consortium")
     for consortium_object in ConsortiumModel.objects.all():
-        consortium_object.country = consortium_object.country_code
+        if consortium_object.country_code:
+            consortium_object.country = consortium_object.country_code
 
-        consortium_object.consortiums_centre.country = consortium_object.country_code
-        consortium_object.save()
+            consortium_object.consortiums_centre.country = consortium_object.country_code
+            consortium_object.save()
 
-        consortium_object.consortiums_centre.all().update(country=consortium_object.country_code)
+            consortium_object.consortiums_centre.all().update(country=consortium_object.country_code)
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('centre_registry', '0049_alter_centre_type_certification_status_fk'),
+        ('centre_registry', '0049_alter_centre_consortium'),
     ]
 
     operations = [
