2389b1be58932090675df2c50fa80d1a3a3f8ef3
MichalGawor <michal.j.gawor@gmail.com>
2026-07-29 15:43:54 +0200

    fk to fks for M2M field

--- centre-registry-app/centre_registry/models.py
+++ centre-registry-app/centre_registry/models.py
@@ -262,9 +262,9 @@ class Centre(Model):
 
     type = ManyToManyField(to=CentreType, verbose_name='Type', related_name='centres_of_type_old')
 
-    type_certification_status_fk = ManyToManyField(TypeCertificationStatus,
+    type_certification_status_fks = ManyToManyField(TypeCertificationStatus,
                                                    blank=True,
-                                                   related_name='certified_centres')
+                                                   related_name='certified_centre')
     type_status_comment = CharField(
         verbose_name="Comments about centre's type",
         max_length=100,
