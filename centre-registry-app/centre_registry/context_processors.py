28a79b67c0dacc3edd44fdeafb2acccd3309226a
MichalGawor <michal.j.gawor@gmail.com>
2026-08-12 17:21:17 +0200

    fix snapshot case for context processor

--- centre-registry-app/centre_registry/context_processors.py
+++ centre-registry-app/centre_registry/context_processors.py
@@ -14,21 +14,17 @@ def tracked_by_piwik(request):
 def version(request):
     # pylint: disable=unused-argument
     version = settings.VERSION
-    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-    print(version)
     alpha_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+(-)?a[0-9]+$")
     beta_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+(-)?(b|rc)[0-9]+$")
     production_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+$")
-    print("###################################################################")
     if alpha_regex.match(version):
-        print("1")
         return {"VERSION": version, "INSTANCE": "ALPHA"}
     elif beta_regex.match(version):
-        print("2")
         return {'VERSION': version, "INSTANCE": "BETA"}
     elif production_regex.match(version):
-        print("3")
         return {'VERSION': version, "INSTANCE": ""}
+    else:
+        return {'VERSION': version, "INSTANCE": "SNAPSHOT"}
 
 
 def centre_profile_xsd_url(request):
