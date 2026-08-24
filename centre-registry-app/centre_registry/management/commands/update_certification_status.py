231946968717b5744c1c421198bbbd723501af38
MichalGawor <michal.j.gawor@gmail.com>
2026-08-24 14:48:10 +0200

    test context processor

--- centre-registry-app/centre_registry/management/commands/update_certification_status.py
+++ centre-registry-app/centre_registry/management/commands/update_certification_status.py
@@ -42,7 +42,7 @@ class Command(BaseCommand):
             subject = "Centres certification expired"
             message = "Following centre has their assessment dates expired today:"
             base_url = "https://" + \
-                       "alpha-" if version["INSTANCE"] == "ALPHA" else "beta-" if version["INSTANCE"] == "BETA" else ""
+                       "alpha-" if version(None)["INSTANCE"] == "ALPHA" else "beta-" if version(None)["INSTANCE"] == "BETA" else ""
             base_url += "centres.clarin.eu/admin/centre_registry/centre/{_id}/change/"
 
             outdated_centres_urls = "\n".join(base_url + _id for _id in outdated_centres)
